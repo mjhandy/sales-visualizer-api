a52ecbbad67cba9028e8823941f9fc7bd04b9e9a
Mark J. Handy <mjhandy@outlook.com>
2025-04-14 18:37:11 -0300

    Update index.php

--- sales-visualizer-data/api/get_data/index.php
+++ sales-visualizer-data/api/get_data/index.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '../../../config/db.php';
 
 header("Content-Type: application/json");
 
-$stmt = $conn->query("SELECT * FROM sales_data");
+$stmt = $conn->query("SELECT * FROM sales_data ORDER BY sale_date");
 $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($data);
 
