c6e38d4cef0fee0e6148496571dba22d84e6a251
Mark J. Handy <mjhandy@outlook.com>
2025-04-27 11:28:25 -0300

    Update index.php

--- sales-visualizer-api/api/maders/index.php
+++ sales-visualizer-api/api/maders/index.php
@@ -1,6 +1,10 @@
 <?php
 require_once __DIR__ . '/../../config/db.php';
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Max-Age: 86400'); // 24 hours
 header("Content-Type: application/json");
 
 $stmt = $conn->query("SELECT * FROM sales_data ORDER BY sale_date");
